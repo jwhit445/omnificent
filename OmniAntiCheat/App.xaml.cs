331404b03529e2b44c553992e4893089b84d8c0b
Luke Miletta <lukemiletta@gmail.com>
2020-12-27 22:53:11 -0800

    Initial project commit

---
+++ OmniAntiCheat/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using OmniAntiCheat.Windows;
+using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace OmniAntiCheat {
+	///<summary>Interaction logic for App.xaml</summary>
+	public partial class App : PrismApplication {
+
+		protected override Window CreateShell() {
+			return Container.Resolve<MainWindow>();
+		}
+
+		protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+			containerRegistry.Register<MainWindow>();
+		}
+
+		protected override void ConfigureViewModelLocator() {
+			base.ConfigureViewModelLocator();
+			ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver(viewType => {
+				string vmTypeStr = viewType.FullName + "VM";
+				Type vmType = Type.GetType(vmTypeStr);
+				if(vmType == null) {
+					if(Debugger.IsAttached) {
+						Debugger.Break();
+					}
+				}
+				return vmType;
+			});
+		}
+
+	}
+}
---
+++ OmniAntiCheat/Windows/MainWindow/MainWindowVM.cs
@@ -0,0 +1,13 @@
+﻿using Core.Omni.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OmniAntiCheat.Windows {
+	public class MainWindowVM : ViewModelBase {
+
+		public MainWindowVM() {
+
+		}
+	}
+}
